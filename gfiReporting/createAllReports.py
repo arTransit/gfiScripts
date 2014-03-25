5c67b1cad199d463a95c10ce79dab129ce52228b
arTransit <andrew_ross@bctransit.com>
2014-03-25 14:53:31 -0700

    All reports working with emailing

--- gfiReporting/createAllReports.py
+++ gfiReporting/createAllReports.py
@@ -9,42 +9,49 @@ import os
 import errno
 import argparse
 import datetime
+import calendar
 import generateMSR
 import generateMRSR
-#import gfiConfig
-#import gfiQuery
-#import gfiXLSX
+import generateExceptionReport
+import smtplib
+import email, email.encoders,email.mime.text,email.mime.base
 
 
-systemList = [
-        {'ids':[1,2],'name':"Victoria_Langford"},
-        {'ids':[3],'name':"Whistler"},
-        {'ids':[4],'name':"Squamish"},
-        {'ids':[5],'name':"Nanaimo"},
-        {'ids':[6],'name':"Abbotsford"},
-        {'ids':[7],'name':"Kelowna"},
-        {'ids':[8],'name':"Kamloops"},
-        {'ids':[9],'name':"Prince George"},
-        {'ids':[10],'name':"Cowichan Valley"},
-        {'ids':[11],'name':"Trail"},
-        {'ids':[12],'name':"Comox"},
-        {'ids':[13],'name':"Port Alberni"},
-        {'ids':[14],'name':"Campbell River"},
-        {'ids':[15],'name':"Powell River"},
-        {'ids':[16],'name':"Sunshine Valley"},
-        {'ids':[17],'name':"Vernon"},
-        {'ids':[18],'name':"Penticton"},
-        {'ids':[19],'name':"Chilliwack"},
-        {'ids':[20],'name':"Cranbrook"},
-        {'ids':[21],'name':"Nelson"},
-        {'ids':[22],'name':"Terrace"},
-        {'ids':[23],'name':"Prince Rupert"},
-        {'ids':[24],'name':"Kitimat"},
-        {'ids':[25],'name':"Fort StJohn"}
-    ]
+SMTPSERVER = '10.170.3.119'
+FROM_EMAIL = 'gfiReporting@bctransit.com'
 
-REPORT_BASE_DIRECTORY='C:/Temp/GFIreporting'
+
+REPORT_BASE_DIRECTORY='G:/BusinessIntelligence/Temp'
+#REPORT_BASE_DIRECTORY='C:/Temp/GFIreporting'
 #REPORTBASEDIRECTORY='G:/Public/GFI/GFIreporting'
+
+
+reportingSystemList = [
+        {'ids':[1,2],'name':'Victoria_Langford','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[3],'name':'Whistler','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[4],'name':'Squamish','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[5],'name':'Nanaimo','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[6],'name':'Abbotsford','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[7],'name':'Kelowna','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[8],'name':'Kamloops','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[9],'name':'Prince George','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[10],'name':'Cowichan Valley','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[11],'name':'Trail','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[12],'name':'Comox','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[13],'name':'Port Alberni','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[14],'name':'Campbell River','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[15],'name':'Powell River','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[16],'name':'Sunshine Valley','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[17],'name':'Vernon','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[18],'name':'Penticton','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[19],'name':'Chilliwack','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[20],'name':'Cranbrook','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[21],'name':'Nelson','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[22],'name':'Terrace','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[23],'name':'Prince Rupert','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[24],'name':'Kitimat','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']},
+        {'ids':[25],'name':'Fort StJohn','email':['andrew_ross@bctransit.com','andrew_miller@bctransit.com']}
+    ]
 
 
 def getArgs():
@@ -73,6 +80,26 @@ def makePath(path):
             raise
 
 
+def emailReport(emailTo,emailFrom,emailSubject,emailBody,filepath):
+    emailMsg = email.MIMEMultipart.MIMEMultipart('alternative')
+    emailMsg['Subject'] = emailSubject
+    emailMsg['From'] = emailFrom
+    emailMsg['To'] = ', '.join(emailTo)
+    emailMsg.attach(email.mime.text.MIMEText(emailBody,'html'))
+
+    # attach file
+    filename = os.path.basename(filepath)
+    fileMsg = email.mime.base.MIMEBase('application','octet-stream')
+    fileMsg.set_payload(open(filepath,'rb').read() )
+    email.encoders.encode_base64(fileMsg)
+    fileMsg.add_header('Content-Disposition','attachment;filename=%s' % filename)
+    emailMsg.attach(fileMsg)
+
+    # send email
+    server = smtplib.SMTP(SMTPSERVER)
+    server.sendmail(emailFrom,emailTo,emailMsg.as_string())
+    server.quit()
+
 
 if __name__ == '__main__':
     args = getArgs()
@@ -80,13 +107,41 @@ if __name__ == '__main__':
         print "Arguement error"
         sys.exit(1)
 
+
+
     # make directories
-    for s in systemList:
+    for s in reportingSystemList:
         makePath(REPORT_BASE_DIRECTORY + '/' + s['name'])
 
-    #
+
+
     # Exception Reports
-    #
+
+    print "Generating Monthly Exception Reports"
+    print "  Year:%s\n  Month:%s" % (str(args.year),str(args.month))
+
+    for s in reportingSystemList:
+        sys.stdout.write('. ')
+        sys.stdout.flush()
+        _filename = '%s/%s/MonthlyException_%s_%s.xlsx' % (
+                REPORT_BASE_DIRECTORY,s['name'],str(args.year),''.join(['000',str(args.month)])[-2:])
+        generateExceptionReport.createReport(s['ids'],args.year,args.month,_filename,args.connection)
+
+        emailBody = (
+                '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" '
+                '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd"><html xmlns="http://www.w3.org/1999/xhtml">'
+                '<body style="font-size:12px;font-family:Tahoma">'
+                '<p>Please find attached the <b>%s GFI Monthly Summary Report, %s %s</b>.</p>'
+                '<p>All reports are located here: <i>G:\BusinessIntelligence\Temp</i></p>'
+                '<p>The GFI Reporting Team &lt;gfiReporting@bctransit.com&gt;</p>'
+                '</body></html>' ) % (s['name'],calendar.month_name[args.month],str(args.year))
+        emailReport(
+                s['email'],
+                FROM_EMAIL,
+                'GFI Monthly Exception Report - %s, %s %d' % (s['name'],calendar.month_name[args.month],args.year),
+                emailBody,
+                _filename)
+    print '\n\n'
 
 
     # Monthly Summary Reports (MSR)
@@ -94,7 +149,7 @@ if __name__ == '__main__':
     print "Generating Monthly Summary Reports"
     print "  Year:%s\n  Month:%s" % (str(args.year),str(args.month))
 
-    for s in systemList:
+    for s in reportingSystemList:
         sys.stdout.write('. ')
         sys.stdout.flush()
         _filename = '%s/%s/MonthlySummary_%s_%s.xlsx' % (
@@ -103,17 +158,19 @@ if __name__ == '__main__':
     print '\n\n'
 
 
+
     # Monthly Route Summary Reports (MRSR)
     print "Generating Monthly Route Summary Reports"
     print "  Year:%s\n  Month:%s" % (str(args.year),str(args.month))
 
-    for s in systemList:
+    for s in reportingSystemList:
         sys.stdout.write('. ')
         sys.stdout.flush()
         _filename = '%s/%s/MonthlyRouteSummary_%s_%s.xlsx' % (
                 REPORT_BASE_DIRECTORY,s['name'],str(args.year),''.join(['000',str(args.month)])[-2:])
         generateMRSR.createReport(s['ids'],args.year,args.month,_filename,args.connection)
     print '\n\n'
+
 
 
     print "Completed."
